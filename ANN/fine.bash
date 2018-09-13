85379c8c152b76e3b41adfd35c37be0f9ddd7040
HyunsuLee <hyunspookey@gmail.com>
2018-09-13 12:41:43 +0900

    faster in desktop.
    1. Inhibitory line, long, short, ramp model done
    2. don't know why.

--- ANN/fine.bash
+++ ANN/fine.bash
@@ -1,12 +1,12 @@
-python3 00_2_binary_full_fine_searching.py &&
-python3 01_2_binary_long_fine_searching.py &&
-python3 02_2_binary_short_fine_searching.py &&
-python3 03_2_binary_ramp_fine_searching.py &&
-python3 04_2_Eline_full_fine_searching.py &&
-python3 05_2_Eline_long_fine_searching.py &&
-python3 06_2_Eline_short_fine_searching.py &&
-python3 07_2_Eline_ramp_fine_searching.py &&
-python3 08_2_Iline_full_fine_searching.py &&
+# python3 00_2_binary_full_fine_searching.py &&
+# python3 01_2_binary_long_fine_searching.py &&
+# python3 02_2_binary_short_fine_searching.py &&
+# python3 03_2_binary_ramp_fine_searching.py &&
+# python3 04_2_Eline_full_fine_searching.py &&
+# python3 05_2_Eline_long_fine_searching.py &&
+# python3 06_2_Eline_short_fine_searching.py &&
+# python3 07_2_Eline_ramp_fine_searching.py &&
+# python3 08_2_Iline_full_fine_searching.py &&
 python3 09_2_Iline_long_fine_searching.py &&
 python3 10_2_Iline_short_fine_searching.py &&
 python3 11_2_Iline_ramp_fine_searching.py
