920ff0e9ac94bc22083556459aecb6de76f85f0b
HyunsuLee <hyunspookey@gmail.com>
2018-09-13 16:29:58 +0900

    making bash file to logging models.

---
+++ ANN/logging.bash
@@ -0,0 +1,12 @@
+python3 00_3_binary_full_logging.py &&
+python3 01_3_binary_long_logging.py &&
+python3 02_3_binary_short_logging.py &&
+python3 03_3_binary_ramp_logging.py &&
+python3 04_3_Eline_full_logging.py &&
+python3 05_3_Eline_long_logging.py &&
+python3 06_3_Eline_short_logging.py &&
+python3 07_3_Eline_ramp_logging.py &&
+python3 08_3_Iline_full_logging.py &&
+python3 09_3_Iline_long_logging.py &&
+python3 10_3_Iline_short_logging.py &&
+python3 11_3_Iline_ramp_logging.py
