bfbd6bd73ba32077de600738951b225e2c4aac28
HyunsuLee <hyunspookey@gmail.com>
2019-03-15 13:51:41 +0900

    edited bash file.

--- ANN/fine.bash
+++ ANN/fine.bash
@@ -1,12 +1,16 @@
-# python3 00_2_binary_full_fine_searching.py &&
-# python3 01_2_binary_long_fine_searching.py &&
-# python3 02_2_binary_short_fine_searching.py &&
-# python3 03_2_binary_ramp_fine_searching.py &&
-# python3 04_2_Eline_full_fine_searching.py &&
-# python3 05_2_Eline_long_fine_searching.py &&
-# python3 06_2_Eline_short_fine_searching.py &&
-# python3 07_2_Eline_ramp_fine_searching.py &&
-# python3 08_2_Iline_full_fine_searching.py &&
+python3 00_2_binary_full_fine_searching.py &&
+python3 01_2_binary_long_fine_searching.py &&
+python3 02_2_binary_short_fine_searching.py &&
+python3 03_2_binary_ramp_fine_searching.py &&
+python3 04_2_Eline_full_fine_searching.py &&
+python3 05_2_Eline_long_fine_searching.py &&
+python3 06_2_Eline_short_fine_searching.py &&
+python3 07_2_Eline_ramp_fine_searching.py &&
+python3 08_2_Iline_full_fine_searching.py &&
 python3 09_2_Iline_long_fine_searching.py &&
 python3 10_2_Iline_short_fine_searching.py &&
-python3 11_2_Iline_ramp_fine_searching.py
+python3 11_2_Iline_ramp_fine_searching.py &&
+python3 12_2_Lline_full_fine_searching.py &&
+python3 13_2_Lline_long_fine_searching.py &&
+python3 14_2_Lline_short_fine_searching.py &&
+python3 15_2_Lline_ramp_fine_searching.py
