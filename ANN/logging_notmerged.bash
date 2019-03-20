c25d3850ffe3975eefa34cf6c2d460efe6424002
HyunsuLee <hyunspookey@gmail.com>
2019-03-20 10:39:54 +0900

    bash file created for logging not merged tg line

---
+++ ANN/logging_notmerged.bash
@@ -0,0 +1,10 @@
+python3 04_3_Eline_full_logging_notmerged.py &&
+python3 05_3_Eline_long_logging_notmerged.py &&
+python3 06_3_Eline_short_logging_notmerged.py &&
+python3 07_3_Eline_ramp_logging_notmerged.py &&
+python3 08_3_Iline_full_logging_notmerged.py &&
+python3 09_3_Iline_long_logging_notmerged.py &&
+python3 10_3_Iline_short_logging_notmerged.py &&
+python3 11_3_Iline_ramp_logging_notmerged.py
+
+
